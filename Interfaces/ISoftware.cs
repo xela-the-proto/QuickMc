d76139624155bc3b2a0bf1cae103f6a748f3cfa5
alex <thegamerale97@gmail.com>
2025-06-26 15:16:40 +0200

    some scaffolding

---
+++ Interfaces/ISoftware.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using QuickMC.Json.JsonClasses;
+
+namespace QuickMC.Interfaces;
+
+public interface ISoftware
+{
+    public Task ChooseSoftware();
+
+    public ServerInfo BuildCustomSoftwareServerInfo();
+    
+    public void DownloadFabricMainManifest();
+
+    public void DownloadFabricVersionManifest();
+    
+}
