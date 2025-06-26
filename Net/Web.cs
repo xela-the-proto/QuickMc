0f481170bc37547c0564f67d22f5f743cf0a0754
alex <thegamerale97@gmail.com>
2025-06-26 12:04:13 +0200

    Reformatted Web.cs and removed useless code

--- Net/Web.cs
+++ Net/Web.cs
@@ -17,20 +17,19 @@ namespace QuickMC.Net;
 //what the fuck
 public class Web : IWeb
 {
-    public async Task<object> Download(HttpClient client, ProgressTask task, string url, string version  = null)
+    public async Task<object> Download(HttpClient client, ProgressTask task, string url, string version = null)
     {
         try
         {
-            bool isManifest = false;
-            bool isInfo = false;
+            bool isMainManifest = false;
+            bool isServerVersionManifest = false;
             bool isJar = false;
-            ServerInfo info = null;
-            DownloadManifestStruct manifestEntry = null;
+
             using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
             {
                 Log.Verbose("Checking success in GET");
                 response.EnsureSuccessStatusCode();
-                    
+
                 // Set the max value of the progress task to the number of bytes
                 task.MaxValue(response.Content.Headers.ContentLength ?? 0);
                 // Start the progress task
@@ -42,21 +41,23 @@ public class Web : IWeb
                 if (filename.Contains(".jar"))
                 {
                     isJar = true;
-                }else if (filename.Contains(".json") && filename != "version_manifest_v2.json")
+                }
+                else if (filename.Contains(".json") && filename != "version_manifest_v2.json")
                 {
-                    
-                    isInfo = true;
+
+                    isServerVersionManifest = true;
                 }
                 else
                 {
-                    isManifest = true;
+                    isMainManifest = true;
                 }
-                Log.Information($"Starting download of {filename} ({task.MaxValue} bytes)");
-                
+
+                Log.Verbose($"Starting download of {filename} ({task.MaxValue} bytes)");
+
                 Log.Verbose("Creating streams");
-                using (var contentStream = await response.Content.ReadAsStreamAsync())
-                using (var fileStream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None,
-                           8192, true))
+                var contentStream = await response.Content.ReadAsStreamAsync();
+                using (var fileStream = new FileStream(filename, FileMode.Create, FileAccess.Write,
+                           FileShare.None, 8192, true))
                 {
                     var buffer = new byte[8192];
                     while (true)
@@ -64,7 +65,7 @@ public class Web : IWeb
                         var read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
                         if (read == 0)
                         {
-                            Log.Information($"Download of [u]{filename}[/] [green]completed![/]");
+                            Log.Verbose($"Download of [u]{filename}[/] [green]completed![/]");
                             break;
                         }
 
@@ -73,24 +74,25 @@ public class Web : IWeb
 
                         // Write the read bytes to the output stream
                         await fileStream.WriteAsync(buffer, 0, read);
-                        
+
                     }
 
                     Log.Verbose("Closing filestream and http client");
                     client.Dispose();
                     fileStream.Close();
-                    
-                    //Get the version specific manifest
-                    if (isInfo)
+
+                    //Get the version specific manifest (Ex. 1.21)
+                    if (isServerVersionManifest)
                     {
                         Log.Verbose("File is a manifest");
-                        File.Copy(filename,Logging.path_root + $"/QuickMc/manifests/{filename}"
-                            ,true);
-                        
+                        File.Copy(filename, Logging.path_root + $"/QuickMc/manifests/{filename}"
+                            , true);
+
                         var manifestStruct = Program.jsonParsers.parseMainManifestForVersion(filename);
-                        
+
                         return manifestStruct;
                     }
+
                     //Get the jar file
                     if (isJar)
                     {
@@ -100,81 +102,40 @@ public class Web : IWeb
                         {
                             Directory.CreateDirectory(Logging.path_root + $"/QuickMc/Servers/{guid}");
                         }
+
                         File.Copy(filename, Logging.path_root + $"/QuickMc/Servers/{guid}/{filename}"
-                            ,true);
-                        Log.Debug("copied jar to folder");
+                            , true);
+                        Log.Verbose("copied jar to folder");
                         ServerInfo serverInfo = new ServerInfo()
                         {
                             firstRun = true,
-                            path =  $"{Logging.path_root}/QuickMc/Servers/{guid}",
+                            path = $"{Logging.path_root}/QuickMc/Servers/{guid}",
                             version = version,
                             guid = guid
                         };
                         return serverInfo;
                     }
-                    //
-                    if (isManifest)
+                    if (isMainManifest)
                     {
                         if (!Directory.Exists(Logging.path_root + $"/QuickMc/manifests"))
                         {
                             Directory.CreateDirectory(Logging.path_root + $"/QuickMc/manifests");
                         }
-                        File.Copy(filename, Logging.path_root + $"/QuickMc/manifests/{filename}",true);
+
+                        File.Copy(filename, Logging.path_root + $"/QuickMc/manifests/{filename}", true);
                         Program.manifest = File.ReadAllText(Logging.path_root + $"/QuickMc/manifests/{filename}");
                     }
                 }
+
+                contentStream.Close();
             }
         }
         catch (Exception e)
         {
-            Log.Information(e.Message);
+            Log.Fatal(e.Message);
             throw;
         }
+
         return null;
-    }
-    
-    public async Task DownloadMainManifest(HttpClient client, ProgressTask task, string url)
-    {
-        try
-        {
-            using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
-            {
-                response.EnsureSuccessStatusCode();
-
-                // Set the max value of the progress task to the number of bytes
-                task.MaxValue(response.Content.Headers.ContentLength ?? 0);
-                // Start the progress task
-                task.StartTask();
-
-                var filename = url.Substring(url.LastIndexOf('/') + 1);
-                AnsiConsole.MarkupLine($"Starting download of [u]{filename}[/] ({task.MaxValue} bytes)");
-
-                using (var contentStream = await response.Content.ReadAsStreamAsync())
-                using (var fileStream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None,
-                           8192, true))
-                {
-                    var buffer = new byte[8192];
-                    while (true)
-                    {
-                        var read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
-                        if (read == 0)
-                        {
-                            AnsiConsole.MarkupLine($"Download of [u]{filename}[/] [green]completed![/]");
-                            break;
-                        }
-
-                        // Increment the number of read bytes for the progress task
-                        task.Increment(read);
-
-                        // Write the read bytes to the output stream
-                        await fileStream.WriteAsync(buffer, 0, read);
-                    }
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            Log.Information(e.Message);
-        }
     }
 }
